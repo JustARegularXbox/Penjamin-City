3ae16ada9e61d7c57a96d59cc7a52e714ce08db4
OzanNail <ozan.nail3232@gmail.com>
2024-10-28 18:56:57 +0100

    banger

--- Assets/Scripts/creatureAnimation.cs
+++ Assets/Scripts/creatureAnimation.cs
@@ -1,15 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class creatureAnimation : MonoBehaviour
 {
-    public SkinnedMeshRenderer meshRenderer;
+    public SkinnedMeshRenderer eyesMeshRenderer;
+    public SkinnedMeshRenderer mouthMeshRenderer;
+    public SkinnedMeshRenderer bodyMeshRenderer;
 
+    [SerializeField] private float animationSpeed;
+    private int weightValue;
     private void Start()
     {
+        animationSpeed = 1;
+        SadCreatureAnimation();
+        //PetCreatureAnimation();
+    }
+
+    private void SetBlendShapeWeight(int index, float weight)
+    {
+        eyesMeshRenderer.SetBlendShapeWeight(index, weight);        
+        mouthMeshRenderer.SetBlendShapeWeight(index, weight);
+        bodyMeshRenderer.SetBlendShapeWeight(index, weight);
+    }
+
+    public void SadCreatureAnimation()
+    {
+        for (int weightValue = 0; weightValue <= 100; weightValue += 1/10)
+        {
+            SetBlendShapeWeight(0, weightValue * animationSpeed);
+            //animationSpeed += 1;
+            Debug.Log("loop");
+        }
+        
         
     }
 
-
+    public void PetCreatureAnimation()
+    {
+        SetBlendShapeWeight(1, 100); 
+    }
 }
